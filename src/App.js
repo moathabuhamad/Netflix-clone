e34dbc2d3e293fc7361c17c8da3c6ef9e2b8a110
moathabuhamad <moathabuhamad@outlook.com>
2022-02-16 08:25:20 +0200

    task18

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import Home from "./components/Home/Home.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  const [movies, setMovies] = useState();
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/trending/movie/day?api_key=5d491173ae53fae27d17349f85825862`,
+      );
+
+      const data = await response.json();
+      let capData = [];
+      for (let c of data.results) {
+        c["caption"] = "No Caption";
+        c["isCaption"] = false;
+        capData.push(c);
+      }
+      console.log(capData[0]);
+      setMovies(capData);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+  const addComment = (data, id) => {
+    let addComment = movies.map((movies) => {
+      if (movies.id === id) {
+        movies.caption = data.userCaption;
+        movies.isCaption = !movies.isCaption;
+        return movies;
+      } else return movies;
+    });
+    setMovies(addComment);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <Routes>
+        <Route
+          path="/"
+          element={<Home movies={movies} addComment={addComment} />}
+        />
+        {/* <Route path="/trending" element={<TrendingList />} /> */}
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ModalMovie/ModalMovie.js
@@ -0,0 +1,67 @@
+import React, { useRef } from "react";
+import { Button, Modal, Form } from "react-bootstrap/";
+
+function ModalMovie(props) {
+  const commentRef = useRef();
+  function handleCaption(e) {
+    e.preventDefault();
+    const userCaption = commentRef.current.value;
+    const newData = { ...props.movie, userCaption };
+    props.addComment(newData, props.movie.id);
+    console.log(props.movie);
+  }
+  console.log(props.movie);
+
+  return (
+    <>
+      <Modal
+        show={props.show}
+        onHide={() => {
+          props.handleColse();
+        }}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>{props.movie.title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <img
+            width="100%"
+            src={props.movie.poster_path}
+            alt={props.movie.title}
+          />
+          <p>
+            {props.movie.isCaption ? props.movie.caption : "No Text Provided"}
+          </p>
+          {/* <p>{props.movie.caption}</p> */}
+        </Modal.Body>
+        <Modal.Footer>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Captions:</Form.Label>
+            <Form.Control
+              ref={commentRef}
+              type="textarea"
+              placeholder={
+                props.movie.isCaption
+                  ? props.movie.caption
+                  : "Add Your Caption Here..."
+              }
+            />
+          </Form.Group>
+          <Button
+            className="addBtn"
+            variant="primary"
+            type="submit"
+            onClick={handleCaption}
+          >
+            Add a Caption
+          </Button>
+          <Button variant="secondary" onClick={props.handleColse}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+export default ModalMovie;
